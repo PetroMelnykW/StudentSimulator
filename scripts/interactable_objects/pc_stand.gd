3baf9443dc9e3e38c7ee47e134f7f7619c3b2e8a
Petro <Petromelnyk.w@gmail.com>
2024-02-04 15:15:39 +0200

    Added advanced interacable outline effect

--- scripts/interactable_objects/pc_stand.gd
+++ scripts/interactable_objects/pc_stand.gd
@@ -1,5 +1,8 @@
 extends StaticBody3D
 
+@export var _monitor_meshes : Array[MeshInstance3D] = []
+@export var _pc_meshes : Array[MeshInstance3D] = []
+
 var _is_pc_turn_on : bool = false
 
 func _on_pc_interacted(interactor : Player) -> void:
@@ -14,3 +17,21 @@ func _on_monitor_interacted(interactor : Player) -> void:
 func _show_os(interactor : Player) -> void:
 	var main : Main = get_tree().current_scene
 	main.switch_os(true)
+
+func _on_monitor_focused() -> void:
+	for mesh : MeshInstance3D in _monitor_meshes:
+		mesh.set_layer_mask_value(2, true);
+
+
+func _on_monitor_unfocused() -> void:
+	for mesh : MeshInstance3D in _monitor_meshes:
+		mesh.set_layer_mask_value(2, false);
+
+
+func _on_pc_focused() -> void:
+	for mesh : MeshInstance3D in _pc_meshes:
+		mesh.set_layer_mask_value(2, true);
+
+func _on_pc_unfocused() -> void:
+	for mesh : MeshInstance3D in _pc_meshes:
+		mesh.set_layer_mask_value(2, false);
