f54e311f35e524650ed1e487d06d40005bec7bcf
IvanGrut <xiomi7855d@gmail.com>
2024-02-11 14:22:14 +0200

    fix

--- scripts/interactable_objects/classroom_door.gd
+++ scripts/interactable_objects/classroom_door.gd
@@ -3,10 +3,7 @@ extends StaticBody3D
 
 @export_file("*.tscn") var _scene_to_load : String
 
-<<<<<<< HEAD
-func _on_interactable_interacted(_interactor: Player) -> void:
-	var main_scene: Main = get_tree().current_scene
-=======
+
 @onready var _doorMesh : MeshInstance3D = $"DoorModel/10057_wooden_door_v002"
 @onready var _doorFrame : MeshInstance3D = $"DoorModel/10057_wooden_door_frame_v002"
 
@@ -20,5 +17,4 @@ func _on_interactable_unfocused() -> void:
 
 func _on_interactable_interacted(interactor : Player) -> void:
 	var main_scene : Main = get_tree().current_scene
->>>>>>> 6b565eec1fd2cdc3e7912b4fbe92011cde837926
 	main_scene.change_room(_scene_to_load)
