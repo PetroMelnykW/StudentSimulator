f9211cac4ffa4681dc5d70bfe656e5e3a8af078f
EducationMax <bazyvolyak_mi@fizmat.tnpu.edu.ua>
2024-03-07 22:20:58 +0200

    solved issues with old db user entity, refactoring code of async ui

--- scripts/player/dynamic_rating_panel_ui.gd
+++ scripts/player/dynamic_rating_panel_ui.gd
@@ -1,8 +1,6 @@
 extends Control
 
-const COLLECTION_ID = 'player_stats'
 const COLLECTION_LEADER_ID = 'leaderboard'
-
 
 @onready var _student_panels : Control = $VBoxContainer/MarginContainer/VBoxContainer
 
@@ -10,18 +8,39 @@ func _ready() -> void:
 	_update_students_panel_data()
 
 func _update_students_panel_data() -> void:
+	
 	while true:
 		var result: Array[Dictionary] = await _get_top_player_list()
 		
 		if (result != null):
 			var panel_count = _student_panels.get_child_count()
-			for i in range(panel_count):
-				var student_data = result[i] if i < len(result) else null
-				_update_student_panel(_student_panels.get_child(i), student_data)
+			
+			for index in range(panel_count):	
+				var student_data = result[index] if index < len(result) else null
+				
+				_update_student_panel(
+					_student_panels.get_child(index), 
+					student_data
+				)
 		else:
 			printerr("Students rating data was not recieved")
 			
 		await get_tree().create_timer(3).timeout
+
+func _update_student_panel(panel: Panel, student_data: Variant) -> void:
+	if student_data == null:
+		panel.hide()
+	else:
+		var panel_h_box = panel.get_node("MarginContainer").get_node("HBoxContainer")
+
+		var nameLabel = panel_h_box.get_node("PlayerNameLabel");
+		var scoreLabel = panel_h_box.get_node("PlayerScoreLabel");
+
+		nameLabel.text = student_data.nickname + ":"
+		scoreLabel.text = str(student_data.score)
+		
+		panel.show()
+
 
 func _get_top_player_list() -> Array[Dictionary]:
 	var doc: Dictionary
@@ -44,12 +63,3 @@ func _get_top_player_list() -> Array[Dictionary]:
 		doc.erase(max_value_key)
 		
 	return top_students
-
-func _update_student_panel(panel: Panel, student_data: Variant) -> void:
-	if student_data == null:
-		panel.hide()
-	else:
-		var panel_h_box = panel.get_node("MarginContainer").get_node("HBoxContainer")
-		panel_h_box.get_node("PlayerNameLabel").text = student_data.nickname + ":"
-		panel_h_box.get_node("PlayerScoreLabel").text = str(student_data.score)
-		panel.show()
